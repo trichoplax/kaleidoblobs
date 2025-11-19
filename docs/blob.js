631a2a7938a35f35325936433242446a82475070
trichoplax <xalpohcirt@gmail.com>
2025-11-19 22:16:59 +0000

    Start grey so colour emerges gradually

--- docs/blob.js
+++ docs/blob.js
@@ -18,9 +18,9 @@ export class Blob {
     this.ctx = ctx;
     this.components = [];
     for (let c = 0; c < numberOfComponents; c++) {
-      const red = Math.floor(Math.random() * 256);
-      const green = Math.floor(Math.random() * 256);
-      const blue = Math.floor(Math.random() * 256);
+      const red = 128;
+      const green = 128;
+      const blue = 128;
       this.components.push(
         new Component(
           centre,
